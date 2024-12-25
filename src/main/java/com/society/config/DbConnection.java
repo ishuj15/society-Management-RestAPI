c6f8e2c4fece9e954cebd1b60f233295a59540a9
ishuj15 <Ishu.Jain@watchguard.com>
2024-12-25 15:22:38 +0530

    updated database conncetion

--- src/main/java/com/society/config/DbConnection.java
+++ src/main/java/com/society/config/DbConnection.java
@@ -17,7 +17,7 @@ public class DbConnection {
 						//connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
 						
 						//local host with rds as a database
-						connection = DriverManager.getConnection("app-society-database.czqqok6w4am2.us-east-1.rds.amazonaws.com/test", "admin", "Admin123#");
+						connection = DriverManager.getConnection("jdbc:mysql://app-society-database.czqqok6w4am2.us-east-1.rds.amazonaws.com/test", "admin", "Admin123#");
 
 					} catch (SQLException e) {
 						// Log the exception with a meaningful message
