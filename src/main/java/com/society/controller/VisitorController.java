acddcefd415ac7d0f0a6dc49013dd07548d717bf
ishuj15 <Ishu.Jain@watchguard.com>
2024-12-27 14:16:44 +0530

    updated

--- src/main/java/com/society/controller/VisitorController.java
+++ src/main/java/com/society/controller/VisitorController.java
@@ -33,11 +33,11 @@ public class VisitorController {
     private static final Logger logger = LoggerFactory.getLogger(VisitorController.class); 
 
 	
-	@PostMapping(path="/visitor")
-	public  ResponseEntity<Object> createVisitor(@Valid @RequestBody Visitor visitor) throws ClassNotFoundException, SQLException {
-		visitor.setIdVisitor(Helper.generateUniqueId());
+	@PostMapping(path="/visitor/{userId}")
+	public  ResponseEntity<Object> createVisitor(@Valid  @PathVariable String userId ,@RequestBody Visitor visitor) throws ClassNotFoundException, SQLException {
+		visitor.setUserId(userId);
 		visitorService.createVisitor(visitor);
-		return ApiResponseHandler.buildResponse(ApiResponseStatus.SUCCESS, HttpStatus.OK, ApiMessages.VISITOR_CREATED,  null);
+		return ApiResponseHandler.buildResponse(ApiResponseStatus.SUCCESS, HttpStatus.OK, ApiMessages.VISITOR_CREATED,  visitor);
 
 	}
 	@GetMapping(path="/visitors")
@@ -53,7 +53,7 @@ public class VisitorController {
 
 	}
 	@PatchMapping(path="/visitor/{visitorId}")
-	public  ResponseEntity<Object> updateVisitor()  throws ClassNotFoundException, SQLException{
+	public  ResponseEntity<Object> updateVisitor(@RequestBody Visitor visitor)  throws ClassNotFoundException, SQLException{
 		return ApiResponseHandler.buildResponse(ApiResponseStatus.SUCCESS, HttpStatus.OK, ApiMessages.VISITOR_UPDATED,  null);
 
 	}
@@ -63,17 +63,13 @@ public class VisitorController {
 		return ApiResponseHandler.buildResponse(ApiResponseStatus.SUCCESS, HttpStatus.OK, ApiMessages.VISITOR_DELETED,  null);
 
 	}
-	@GetMapping(path="/visitors/{userId}/")
+	@GetMapping(path="/visitors/{userId}/status")
 	public   ResponseEntity<Object> getAllPendingRequests(@PathVariable String userId, @RequestParam String status)  throws ClassNotFoundException, SQLException {
 		List<Visitor> list= visitorService.getAllPendingRequests(userId,status);
 		return ApiResponseHandler.buildResponse(ApiResponseStatus.SUCCESS, HttpStatus.OK, ApiMessages.FETCHED,  list);
 
 	}
-//	@PatchMapping(path="/visitors/{id}")
-//	public void verifyVisitor() {
-//		
-//	}
-//	
+
 	//pending requests 	
 	//verify visitor
 
